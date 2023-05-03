6418b59ceb38799605f8769cd016ae8d24b5aa82
warren wang <warren@end-game.com>
2023-05-03 18:27:41 +1200

    formatting

--- pages/d3/pie/simple-pie/index.tsx
+++ pages/d3/pie/simple-pie/index.tsx
@@ -1,27 +1,27 @@
-import Layout from "../../../../components/Layout/Layout";
-import Pie from "../../../../components/Pie/Pie";
+import Layout from '../../../../components/Layout/Layout'
+import Pie from '../../../../components/Pie/SimplePie'
 
 const PiePage = () => {
   const data = [
-    { label: "A", value: 3 },
-    { label: "B", value: 2 },
-    { label: "C", value: 1 },
-    { label: "D", value: 2 },
-  ];
+    { label: 'A', value: 3 },
+    { label: 'B', value: 2 },
+    { label: 'C', value: 1 },
+    { label: 'D', value: 2 },
+  ]
   return (
     <Layout>
       <Pie
         pieData={data}
-        valueAttribute={"value"}
-        textAttribute={"label"}
-        width={"700px"}
+        valueAttribute={'value'}
+        textAttribute={'label'}
+        width={'700px'}
         innerRadius={0}
         outerRadius={300}
         padRadius={300}
         cornerRadius={0}
       />
     </Layout>
-  );
-};
+  )
+}
 
-export default PiePage;
+export default PiePage
