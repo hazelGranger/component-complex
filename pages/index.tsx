effe894c6bb1c943b100d85084d03258d244ac34
hazelGranger <wangzhen_blcu@163.com>
2023-05-08 20:08:57 +1200

    home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,8 @@
+import Layout from "../components/Layout/Layout";
+
 export default function Home() {
-  return <>Components</>
+  return <>
+  <h1>Complex Components in React</h1>
+  <p> This is my collections for building complex UI components in React</p>
+  </>
 }
